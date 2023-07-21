28fc098b51e4b35de04bab788efbfbbef7fba1d9
TonyCross23 <tonychamp40@gmail.com>
2023-07-22 00:49:31 +0630

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNav />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import SiteNav from "./components/SiteNav.vue";
+</script>
